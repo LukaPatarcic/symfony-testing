591f0e81cf2cb496355980aae63e74ae75e7d3ca
Luka <patarcic98@gmail.com>
2019-05-18 13:02:02 +0200

    Even More Cleanup hehe

--- wallet-symfony/public/assets/js/main.js
+++
@@ -1,8 +0,0 @@
-var pathname = window.location.pathname;
-var match = /\/user\/dashboard\/((\w)*)/.exec(pathname);
-if(match[1] == '') {
-    $('#home').addClass('active');
-} else {
-    $('#'+match[1]).addClass('active');
-
-}
